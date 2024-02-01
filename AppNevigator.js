a9f007e394f0512f32f6815c52f44bdf5dc0557b
Maor Hadad <maorhadad94@gmail.com>
2024-02-01 17:09:06 +0200

    dev-back

---
+++ AppNevigator.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+
+const Stack = createNativeStackNavigator(){
+    return (
+        <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Profile" component={ProfileScreen} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+        </NavigationContainer>
+    );
+}
